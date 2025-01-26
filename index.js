8b33821f90a7f5f461c137d6ff42db45c3d4cdb1
iam1513 <126111227+iam1513@users.noreply.github.com>
2025-01-26 13:12:29 +0530

    FEAT : Set up of proxy middleware. Checked if user is suthenticated or not before redirecting it to a service use express middleware.

---
+++ index.js
@@ -0,0 +1,52 @@
+const express = require("express")
+const morgan = require('morgan')
+const { createProxyMiddleware } = require('http-proxy-middleware')
+const rateLimit = require('express-rate-limit')
+const axios = require("axios")
+const app = express();
+
+const PORT = 3004
+
+const limiter = rateLimit({
+    windowMs: 2 * 60 * 100,
+    max: 5
+})
+
+
+app.use(morgan('combined'))
+app.use(limiter)
+
+app.use('/bookingservice', async (req, res, next) => {
+    console.log(req.headers['x-access-token'])
+    try {
+        const response = await axios.get('http://localhost:3001/api/v1/isauthenticated', {
+            headers: {
+                'x-access-token': req.headers['x-access-token']
+            }
+        })
+
+        if (response.data.success) {
+            next()
+        }
+        else {
+            return res.status(401).json({
+                message: "Unauthorised"
+            })
+        }
+    } catch (error) {
+        return res.status(401).json({
+            message: "Unauthorised"
+        })
+    }
+})
+
+app.use('/bookingservice', createProxyMiddleware({ target: 'http://localhost:3002', changeOrigin: true }))
+app.get("/home", (req, res) => {
+    return res.json({
+        msg: "ok"
+    })
+})
+
+app.listen(PORT, () => {
+    console.log(`Server up and running on ${PORT}`)
+})
